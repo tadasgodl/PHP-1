477141a6ed2c2f4bc0ec1ce95e727c3784c6047e
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 14:48:07 +0300

    14:47 bomb task#6

--- index.php
+++ index.php
@@ -3,33 +3,19 @@
 <head>
     <title>Bomb</title>
     <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1">
     <style>
         body {
             display: flex;
             justify-content: center;
         }
-        .dice1 {
-            content: url("images/dice1.png");
+        div {
+            content: url("images/bomb.png");
+            height: <?php print date('s') . 'px';?>;
         }
-        .dice2 {
-            content: url("images/dice2.png");
-        }
-        .dice3 {
-            content: url("images/dice3.png");
-        }
-        .dice4 {
-            content: url("images/dice4.png");
-        }
-        .dice5 {
-            content: url("images/dice5.png");
-        }
-        .dice6 {
-            content: url("images/dice6.png");
-        }
-
     </style>
 </head>
 <body>
-    <div class="dice<?php print rand(1, 6);?>"></div>
+    <div></div>
 </body>
 </html>
