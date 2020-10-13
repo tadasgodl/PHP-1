0a6577a7b6675d049e370bd393b121b91fd2e130
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 12:00:59 +0300

    12:00 task#4

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>PHP lydes ir <?php print date('l', strtotime('+1 day')) . '!'; ?></title>
+    <title>
+        PHP lydes ir
+        <?php print date('y-m-d', strtotime('+' . rand(1, 1000) . 'days')) . '!'; ?></title>
     <meta charset="UTF-8">
 </head>
 <body>
     <main>
         <h1>
-            <b>Vardas</b> - PHP su manim buvo ir
-            <?php print date('h', strtotime('-1 hour')) . ' valanda!'; ?>
+            <b>Vardas</b> - galbut turesiu
+            <?php print rand(1, 5) . ' vaiku(us)!'; ?>
         </h1>
         <p>
-            <?php print date('Y', strtotime('+1 year')); ?> ne uz kalnu!
+            Trumpas nebebus prezidentu:
+            <?php print date('Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?> ne uz kalnu!
         </p>
     </main>
 </body>
