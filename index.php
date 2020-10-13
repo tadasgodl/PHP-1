cafaf3ed531368ff7c00e7e085db9e1f0ebc317f
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 10:08:20 +0300

    10:08 #task 2 correction

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>As valdau HTML <?php print('ir PHP')?>!</title>
+    <title>As valdau HTML <?php print('ir PHP');?>!</title>
     <meta charset="UTF-8">
 </head>
 <body>
     <main>
-        <h1>Tadas<span style="font-weight: normal; font-size: 30px;"> - HTML <?php print('ir PHP')?> asas!</span></h1>
-        <p>Moku kurti statini kurini.<br>Atidaryta: <?php print date('y-m-d h:i:m')?></p>
+        <h1>Tadas<span style="font-weight: normal; font-size: 30px;"> - HTML <?php print('ir PHP');?> asas!</span></h1>
+        <p>Moku kurti statini kurini.<br>Atidaryta: <?php print date('y-m-d H:i:s');?></p>
     </main>
 </body>
 </html>
