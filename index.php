28d7279f94435a26977b1be5598384642fd2e24c
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 09:42:22 +0300

    09:42

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-print 'veikia!';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>As vauldau HTML!</title>
+</head>
+<body>
+    <main>
+        <h1>Tadas<span style="font-weight: normal; font-size: 30px;"> - HTML asas!</span></h1>
+        <p>Moku kurti statini kurini.</p>
+    </main>
+
+</body>
+</html>
