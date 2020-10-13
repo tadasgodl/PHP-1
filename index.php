4ce626179fc367d0a22515c4381ad0e674eca1ab
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 10:04:01 +0300

    10:03 task #2

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>As valdau HTML!</title>
+    <title>As valdau HTML <?php print('ir PHP')?>!</title>
+    <meta charset="UTF-8">
 </head>
 <body>
     <main>
-        <h1>Tadas<span style="font-weight: normal; font-size: 30px;"> - HTML asas!</span></h1>
-        <p>Moku kurti statini kurini.</p>
+        <h1>Tadas<span style="font-weight: normal; font-size: 30px;"> - HTML <?php print('ir PHP')?> asas!</span></h1>
+        <p>Moku kurti statini kurini.<br>Atidaryta: <?php print date('y-m-d h:i:m')?></p>
     </main>
-
 </body>
 </html>
