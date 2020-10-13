4d35ff0e16429bf3ba62a680dadbd5507dd03140
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-13 11:27:22 +0300

    11:21 task#3

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php print 'As, ' . date('l') . ' ir PHP'?></title>
+    <title>PHP lydes ir <?php print date('l', strtotime('+1 day')) . '!'; ?></title>
     <meta charset="UTF-8">
 </head>
 <body>
     <main>
         <h1>
-            Vardas
-            <span style="font-weight: normal">
-                - HTML <?php print 'ir PHP'; ?> asas jau nuo
-                <?php print date('Y') . ' metu'; ?>
-            </span>
+            <b>Vardas</b> - PHP su manim buvo ir
+            <?php print date('h', strtotime('-1 hour')) . ' valanda!'; ?>
         </h1>
         <p>
-            Viskas prasidejo
-            <?php print date('m') . ' menesio ' . date('d') . ' diena!'; ?>
+            <?php print date('Y', strtotime('+1 year')); ?> ne uz kalnu!
         </p>
     </main>
 </body>
