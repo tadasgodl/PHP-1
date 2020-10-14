01a93040d0f0d3379cb4cacb796f80e088cdcf30
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-14 10:12:12 +0300

    2020-10-14 task#2

--- index.php
+++ index.php
@@ -1,23 +1,18 @@
 <?php
-    $string = 'string';
-    $int = 10;
-    $bool = true;
-    $null = null;
+    $img_src = 'images/dice1.png';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Variables</title>
     <meta charset="UTF-8">
-    <style>
-    </style>
 </head>
 <body>
-    <ul>
-        <li>String Tipas:<?php print $string; ?></li>
-        <li>Integer Tipas:<?php print $int; ?></li>
-        <li>Boolean Tipas:<?php print $bool; ?></li>
-        <li>Null Tipas:<?php print $null; ?></li>
-    </ul>
+    <div>
+        <img src="<?php print $img_src; ?>" alt="">
+        <img src="<?php print $img_src; ?>" alt="">
+        <img src="<?php print $img_src; ?>" alt="">
+        <img src="<?php print $img_src; ?>" alt="">
+    </div>
 </body>
 </html>
