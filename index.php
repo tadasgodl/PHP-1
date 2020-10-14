b5998310b4908c24d36d24416bbd674629f17043
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-14 14:55:06 +0300

    2020-10 14 second-part start | task#1

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php
-    $grizai_isgeres = 0;
-    $grizai_velai = 1;
+    $random_card_1_player = rand(1, 11);
+    $random_card_2_player = rand(1, 11);
+    $random_card_1_dealer = rand(1, 11);
+    $random_card_2_dealer = rand(1, 11);
 
-    if ($grizai_velai && $grizai_isgeres) {
-        $outcome = 'Grizai velai ir isgeres';
-    } else if ($grizai_velai) {
-        $outcome = 'Grizai velai';
-    } else if ($grizai_isgeres) {
-        $outcome = 'Grizai isgeres';
+    $player_hand = $random_card_1_player + $random_card_2_player;
+    $dealer_hand = $random_card_1_dealer + $random_card_2_dealer;
+
+    if ($player_hand < $dealer_hand) {
+        $winner = 'Dealer';
+        $winner_hand = 'with ' . $dealer_hand;
     } else {
-        $outcome = 'Nieko nepadarei';
+        if ($player_hand == $dealer_hand) {
+            $winner = 'No one';
+            $winner_hand = null;
+        } else {
+            $winner = 'Player';
+            $winner_hand = 'with ' . $player_hand;
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -19,9 +27,16 @@
     <meta charset="UTF-8">
 </head>
 <body>
+    <h1><?php print $winner; ?> is the winner <?php print $winner_hand; ?></h1>
     <div>
-        <h1>Buitine skaiciuokle</h1>
-        <h2><?php print $outcome; ?></h2>
+        <h2>Dealer gets <?php print $dealer_hand; ?></h2>
+        <div><?php print $random_card_1_dealer; ?></div>
+        <div><?php print $random_card_2_dealer; ?></div>
+    </div>
+    <div>
+        <h2>Player gets <?php print $player_hand; ?></h2>
+        <div><?php print $random_card_1_player; ?></div>
+        <div><?php print $random_card_2_player; ?></div>
     </div>
 </body>
 </html>
