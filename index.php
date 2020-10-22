379fdfd7b0163e9e476703deb45ae003ccb1f6b3
tadasgodl <71317716+tadasgodl@users.noreply.github.com>
2020-10-22 12:17:30 +0300

    2020-10-22 foreach task#1

--- index.php
+++ index.php
@@ -1,47 +1,35 @@
 <?php
-$work_time = 12;
-$max_capacity = 10; // 10 customers max at a time //
-$total_customers = 0;
-$total_profit = 0;
-$lost_customers = 0;
-$carusel_cycle = 0;
-$ticket_price = 13; // eur //
+$my_memories = [
+        'Taksi',
+        'Baras',
+        'Laukas',
+        'Gatve',
+        'Valgis',
+        'Gerimai',
+        'Pinigai'
+];
 
-for ($x = 1; $x <= $work_time; $x++) {
-    $hourly_intake = rand(1, 20);
-    if ($hourly_intake <= $max_capacity) {
-        if ($hourly_intake == $max_capacity) { // 10 customers take the ride and are added to client list //
-            $total_customers += 10;
-            $carusel_cycle++;
-        } else {
-            $lost_customers += $hourly_intake;
-            $total_customers += 0; // less than 10 customers appear, ride doesn't start //
-        }
-    } else { // more than 10 costumers, 10 is max capacity, others left //
-        $lost_customers += ($hourly_intake - 10);
-        $total_customers += 10;
-        $carusel_cycle++;
-    }
-    $total_profit = $total_customers * $ticket_price;
-}
+$fb_index = array_rand($my_memories);
+$fb_text = $my_memories[$fb_index];
 
-$h1 = "Carusel profit is $total_profit euros.";
-$h2 = "$total_customers people enjoyed the ride in the carusel today.";
-$h3 = "$lost_customers people left for the carusel couldn't start in time.";
-$h4 = "Carusel has had $carusel_cycle cycles today."
+$h3 = "$fb_index: $fb_text";
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Loops</title>
+    <title>ForEach Cycle</title>
 </head>
 <body>
     <main>
-        <h1><?php print $h1; ?></h1>
-        <h2><?php print $h2; ?></h2>
+        <h1>Kas buvo penktadieni?</h1>
+        <h2>Mano prisiminimai: </h2>
         <h3><?php print $h3; ?></h3>
-        <h4><?php print $h4; ?></h4>
+        <ul>
+            <?php foreach ($my_memories as $my_memory): ?>
+                <li><?php print $my_memory; ?></li>
+            <?php endforeach; ?>
+        </ul>
     </main>
 </body>
 </html>
